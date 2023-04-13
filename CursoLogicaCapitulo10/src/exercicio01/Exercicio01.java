bb5b1d99fab8264f3710d5365488b8fe0c25ccfe
Vinícius Ricarte <128393426+Ricarte13@users.noreply.github.com>
2023-04-12 21:01:08 -0300

    Add files via upload

---
+++ CursoLogicaCapitulo10/src/exercicio01/Exercicio01.java
@@ -0,0 +1,46 @@
+package exercicio01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercicio01 {
+	
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+	
+		ArrayList<String> linhas = new ArrayList<String>();
+		
+		String tarefa;
+		int i = 0;
+		while (true) {
+			System.out.print("Tarefa " + i + ": ");
+			tarefa = scanner.nextLine();
+			
+			if ("x".equals(tarefa)) {
+				break; 
+			}
+	
+			linhas.add(tarefa);
+			i++;
+		}
+	
+		escreverNoArquivo("C:\\\\\\\\Users\\\\\\\\vinic\\\\\\\\eclipse-workspace\\\\\\\\Aula10\\\\\\\\Exercicio-01.txt", linhas);
+	
+		scanner.close();
+		
+		System.out.println("Fim...");
+	}
+	
+	static void escreverNoArquivo(String arquivo, ArrayList<String> linhas) throws IOException {
+		Path path = Paths.get(arquivo);
+		Files.write(path, linhas);
+	}
+	
+	
+	
+}
