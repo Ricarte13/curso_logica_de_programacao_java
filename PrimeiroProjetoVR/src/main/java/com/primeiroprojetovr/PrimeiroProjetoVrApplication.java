2e5504ca52e381deeb38de2c7a8aa91d586fa92a
Vinícius Ricarte <128393426+Ricarte13@users.noreply.github.com>
2023-04-24 20:51:03 -0300

    Add files via upload

---
+++ PrimeiroProjetoVR/src/main/java/com/primeiroprojetovr/PrimeiroProjetoVrApplication.java
@@ -0,0 +1,13 @@
+package com.primeiroprojetovr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoVrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoVrApplication.class, args);
+	}
+
+}
